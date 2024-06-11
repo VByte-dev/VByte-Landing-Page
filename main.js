8d682d3f756a1db5ab3df3b06cebd9a008201489
Vedhesh Narayanan <135724403+VByte-dev@users.noreply.github.com>
2024-06-11 18:44:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+// Navbar
+let navButton = document.querySelector('#navButton');
+navButton.addEventListener('click', navButtonClick);
 
+let countClick = 0;
+
+function navButtonClick() {
+    countClick += 1;
+
+    if(countClick%2 == 0){
+        navButton.style.animation = '0.2s navButtonAnim linear 0s';
+        let navIcons = document.querySelector('#navIcons');
+        navIcons.style.display = 'block';
+    }
+    else{
+        let navIcons = document.querySelector('#navIcons');
+        navIcons.style.display = 'none';
+        navButton.style.animation = 'none';
+    }
+}
